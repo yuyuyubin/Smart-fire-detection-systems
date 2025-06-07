24a18242a14ac975ca14bf8b9c677bc931bc004b
yuyuyubin <129951226+yuyuyubin@users.noreply.github.com>
2025-06-07 13:40:50 +0900

    real last

--- fire_detection_server/app.py
+++ fire_detection_server/app.py
@@ -286,4 +286,4 @@ def sensors_graph_data():
     return jsonify(data), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
